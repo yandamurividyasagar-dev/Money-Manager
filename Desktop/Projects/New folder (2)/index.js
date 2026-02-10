fba2d7ed6fabbe926c8a58256c7c218ddac63e39
Vidyasagar <yandamurividyasagar@gmai.com>
2026-02-10 12:58:07 +0530

    firstCommit

---
+++ Desktop/Projects/New folder (2)/index.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    var name = document.getElementById("nameInput").value;
+
+    document.getElementById("result").innerHTML =
+        "Hello " + name + "! Welcome 😊";
+}
